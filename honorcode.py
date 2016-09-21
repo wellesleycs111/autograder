74e67e91bcbd54aec13a042a65b6730c821f1d18
Sravana Reddy <sravana@cs.dartmouth.edu>
2016-09-21 15:41:52 -0400

    useful to have honorcode here

---
+++ honorcode.py
@@ -0,0 +1,30 @@
+"""
+By submitting this assignment,
+I certify that our code is entirely written by me (and my partner for partner tasks),
+adhering to Wellesley's Honor Code.
+"""
+
+# Replace the empty strings with the appropriate values before you submit.
+# Your username is your Wellesley e-mail username (not including @wellesley.edu)
+
+YourFullName = ""
+YourUsername = ""
+
+PartnerFullName = ""
+PartnerUsername = ""
+
+TimeReading = 0 # time in hours spent on general reading
+TimeTask1 = 1  # time in hours spent on question 1
+TimeTask2 = 2  # time in hours spent on question 2
+
+# Adhering to Wellesley's Honor Code, how did you write your code?
+# Enter 1, 2, 3, or 4 for HowWeWorked:
+# 1. We talked exclusively to each other and used the 111 website, with no external help
+# 2. We received help from CS111 Staff (name people in "Sources")
+# 3. We consulted external sources. (name in "Sources")
+# 4. Both 2 and 3: we received help from staff and consulted external sources. (name in "Sources")
+HowWeWorked = 0
+
+# If you received help from CS111 Staff OR consulted external sources,
+# list the people or sources below; otherwise, write "no help".
+Sources = ""
